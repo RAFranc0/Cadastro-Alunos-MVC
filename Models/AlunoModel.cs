765d5867589589a83d95d4999c592ad80b65c973
Raul <raul.franco266@gmail.com>
2026-04-28 22:49:08 -0300

    feat: criar AlunoModel

---
+++ Models/AlunoModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CadastroAlunosMVC.Models;
+
+public class AlunoModel
+{
+    public Guid Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public DateTime DataNascimento { get; set; }
+}
